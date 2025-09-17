877bf6cf837f2c4f222cdb9d3311c16e2f957994
Nathan Fisher <na.fisheruk@gmail.com>
2025-09-17 20:36:53 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
